a0b8426b1064a9651b081ee0c799a9108c6e5f41
Abhibagad <abhishekbagad9@gmail.com>
2022-12-28 07:27:00 +0530

    [Abhibagad]:Check Employee Present Or Apsent

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+
+namespace EmployeeWageComputationProblem
+{
+    Console.WriteLine("Welcome to Employee Wage Computation Problem!");
+    public class PresentApsent
+    {
+        public static void main(string[] args)
+        {
+            Random r = new Random();
+            int genRand = r.Next(0, 2);
+            if (genRand == 0)
+            {
+                Console.WriteLine("EMP is PRESENT");
+            }
+            else
+            {
+                Console.WriteLine("EMP IS APSENT");
+            }
+            Console.WriteLine("Random Number = " + genRand);
+        }
+
+
+
+    }
